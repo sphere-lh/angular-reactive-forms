a67429b67e9646d7bcfefe4a435423639dbf8f89
Luis Mesajil <lhmesajil@gmail.com>
2024-11-20 22:23:09 -0500

    feat(form): Update form to use reactive form | Add error handling

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,9 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatError, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 
@@ -12,31 +17,40 @@ import { MatSelectModule } from '@angular/material/select';
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
-    FormsModule,
     MatInputModule,
     MatSelectModule,
     MatCheckboxModule,
     MatButtonModule,
+    MatError,
   ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
 })
-export class FormComponent {
-  title = 'My form';
-
-  // Modelo para capturar los valores del formulario
-  formData = {
-    textInput: 'Omar',
-    selectedOption: '',
-    isChecked: false,
-  };
+export class FormComponent implements OnInit {
+  title = 'My Reactive form';
 
   // Opciones para el select
   options = ['Perú', 'Chile', 'Argentina'];
 
+  // Modelo para capturar los valores del formulario
+  form: FormGroup = new FormGroup({});
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      textInput: new FormControl('Omar', [Validators.required]),
+      selectedOption: new FormControl('', [Validators.required]),
+      isChecked: new FormControl(false),
+    });
+  }
+
   // Método para guardar y mostrar en la consola los valores del formulario
   saveForm() {
-    console.log('Form Data:', this.formData);
+    if (this.form.valid) {
+      console.log('Form is valid:', this.form.value);
+    } else {
+      console.log('Form is invalid:', this.form.value);
+    }
   }
 }
